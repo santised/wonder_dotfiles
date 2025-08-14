a31310a697ba0027122fb25df3ed121638a1f52b
Elias Santistevan <elias.santistevan@proton.me>
2025-08-13 21:09:04 -0600

    Adds some substring matching to picom opacity rules, adds starship toml

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt_local.tabstop = 4
-vim.opt_local.softtabstop = 4
-vim.opt_local.shiftwidth = 4
-vim.opt_local.expandtab = true
+vim.opt_global.tabstop = 4
+vim.opt_global.softtabstop = 4
+vim.opt_global.shiftwidth = 4
+vim.opt_global.expandtab = true
+vim.opt_global.smarttab = true
